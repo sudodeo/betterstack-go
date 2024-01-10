85c7282312648edd73068ec2811b3fcb6bbe0787
sudodeo <adesolaadeoluwa@gmail.com>
2024-01-10 13:57:42 +0100

    add test file for log queries

---
+++ sdk/logs/query_test.go
@@ -0,0 +1,16 @@
+package logs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/sudodeo/betterstack-go/models/logs"
+)
+
+func TestFetchLogs(t *testing.T) {
+	_logs, err := bs.FetchLogs()
+	assert.Nil(t, err)
+	assert.NotEmpty(t, _logs)
+
+	assert.IsType(t, &logs.FetchLogsResponse{}, _logs)
+}
