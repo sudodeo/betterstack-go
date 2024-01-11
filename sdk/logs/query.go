4d5300b6cfb93ea3f72f55c9b8cdd2ef715e2de3
sudodeo <adesolaadeoluwa@gmail.com>
2024-01-12 00:51:52 +0100

    add queryParams

--- sdk/logs/query.go
+++ sdk/logs/query.go
@@ -1,15 +1,27 @@
 package logs
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/sudodeo/betterstack-go/models/logs"
 )
 
 // FetchLogs fetches logs from the Betterstack API.
-func (bs *Betterstack) FetchLogs() (*logs.Logs, error) {
-	req, err := http.NewRequest("GET", "/api/v1/query", nil)
+func (bs *Betterstack) FetchLogs(queryParams *logs.FetchLogsParams) (*logs.Logs, error) {
+	var reqBody io.Reader
+
+	if queryParams != nil {
+		reqBodyBytes, err := json.Marshal(queryParams)
+		if err != nil {
+			return nil, err
+		}
+		reqBody = bytes.NewReader(reqBodyBytes)
+	}
+
+	req, err := http.NewRequest(http.MethodGet, "/api/v1/query", reqBody)
 	if err != nil {
 		return nil, err
 	}
