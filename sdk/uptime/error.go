7c8381bfa64c1a0e72bfa291db0a0b5ab270cb52
sudodeo <adesolaadeoluwa@gmail.com>
2024-01-12 02:01:28 +0100

    add entry file for uptime sdk

---
+++ sdk/uptime/error.go
@@ -0,0 +1,13 @@
+package uptime
+
+import "fmt"
+
+// BetterStackError represents an error returned by the BetterStack SDK.
+type BetterStackError struct {
+	StatusCode int    // The HTTP status code associated with the error.
+	Message    string // The error message.
+}
+
+func (err *BetterStackError) Error() string {
+	return fmt.Sprintf(`Error failed with status code: %d\nerror: %v`, err.StatusCode, err.Message)
+}
