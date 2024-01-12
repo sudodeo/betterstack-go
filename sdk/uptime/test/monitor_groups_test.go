3ea3686fa639571a0400abd715b0da041a5e86a4
sudodeo <adesolaadeoluwa@gmail.com>
2024-01-12 08:17:17 +0100

    add test file for monitor groups

---
+++ sdk/uptime/test/monitor_groups_test.go
@@ -0,0 +1,60 @@
+package uptime_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/sudodeo/betterstack-go/models/uptime"
+)
+
+var monitorGroupID string
+
+func TestListMonitorGroups(t *testing.T) {
+	monitorGroups, err := bs.ListMonitorGroups()
+	assert.Nil(t, err)
+	assert.NotNil(t, monitorGroups)
+	assert.IsType(t, &uptime.Groups{}, monitorGroups)
+}
+
+func TestCreateMonitorGroup(t *testing.T) {
+	reqBody := uptime.GroupReqBody{
+		Name: "test2_group",
+	}
+	monitorGroup, err := bs.CreateMonitorGroup(reqBody)
+	assert.Nil(t, err)
+	assert.NotNil(t, monitorGroup)
+	assert.IsType(t, &uptime.Group{}, monitorGroup)
+	monitorGroupID = monitorGroup.ID
+}
+
+func TestGetAllMonitorsInGroup(t *testing.T) {
+	monitors, err := bs.GetAllMonitorsInGroup(monitorGroupID)
+	assert.Nil(t, err)
+	assert.NotNil(t, monitors)
+	assert.IsType(t, &uptime.Monitors{}, monitors)
+}
+
+func TestGetMonitorGroup(t *testing.T) {
+	monitorGroup, err := bs.GetMonitorGroup(monitorGroupID)
+	assert.Nil(t, err)
+	assert.NotNil(t, monitorGroup)
+	assert.IsType(t, &uptime.Group{}, monitorGroup)
+	assert.Equal(t, monitorGroupID, monitorGroup.ID)
+}
+
+func TestUpdateMonitorGroup(t *testing.T) {
+	reqBody := uptime.GroupReqBody{
+		Name:   "update test group",
+		Paused: true,
+	}
+	monitorGroup, err := bs.UpdateMonitorGroup(monitorGroupID, reqBody)
+	assert.Nil(t, err)
+	assert.NotNil(t, monitorGroup)
+	assert.IsType(t, &uptime.Group{}, monitorGroup)
+	assert.Equal(t, monitorGroupID, monitorGroup.ID)
+}
+func TestDeleteMonitorGroup(t *testing.T) {
+	err := bs.DeleteMonitorGroup(monitorGroupID)
+	assert.Nil(t, err)
+}
