a20a1b4c4392ea7d825b89fd9165deb852ba72c9
sudodeo <adesolaadeoluwa@gmail.com>
2024-01-13 23:01:06 +0100

    add test file for status page resources

---
+++ sdk/uptime/test/status_resources_test.go
@@ -0,0 +1,86 @@
+package uptime_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/sudodeo/betterstack-go/models/uptime"
+)
+
+var (
+	statusPageResourceID string
+)
+
+func TestCreateStatusPageResource(t *testing.T) {
+	monitor, err := bs.CreateMonitor(
+		uptime.MonitorReqBody{
+			URL: "https://google.com",
+		},
+	)
+	assert.Nil(t, err)
+	assert.NotNil(t, monitor)
+	monitorID = monitor.ID
+	reqBody := uptime.StatusPageResourceReqBody{
+		ResourceType: "Monitor",
+		ResourceID:   monitorID,
+		PublicName:   "test_resource",
+	}
+
+	newStatusPage, err := bs.CreateStatusPage(uptime.StatusPageReqBody{
+		CompanyName: "test_company_update",
+		Subdomain:   "test-subdomain-section",
+		Timezone:    "Casablanca",
+	})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, newStatusPage)
+	newStatusPageID = newStatusPage.ID
+	statusPageResource, err := bs.CreateStatusPageResource(newStatusPageID, reqBody)
+	assert.Nil(t, err)
+	assert.NotNil(t, statusPageResource)
+	assert.IsType(t, &uptime.StatusPageResource{}, statusPageResource)
+	assert.Equal(t, reqBody.PublicName, statusPageResource.Attributes.PublicName)
+	assert.Equal(t, reqBody.ResourceID, strconv.Itoa(statusPageResource.Attributes.ResourceID))
+	statusPageResourceID = statusPageResource.ID
+}
+
+func TestListStatusPageResources(t *testing.T) {
+	// Test case 1: Successful API response
+	statusPageResources, err := bs.ListStatusPageResources(newStatusPageID)
+	assert.Nil(t, err)
+	assert.NotNil(t, statusPageResources)
+	assert.IsType(t, &uptime.StatusPageResources{}, statusPageResources)
+}
+
+func TestGetStatusPageResource(t *testing.T) {
+	statusPageResource, err := bs.GetStatusPageResource(newStatusPageID, statusPageResourceID)
+	assert.Nil(t, err)
+	assert.NotNil(t, statusPageResource)
+	assert.IsType(t, &uptime.StatusPageResource{}, statusPageResource)
+	assert.Equal(t, statusPageResourceID, statusPageResource.ID)
+}
+
+func TestUpdateStatusPageResource(t *testing.T) {
+	reqBody := uptime.StatusPageResourceReqBody{
+		PublicName:   "test_resource_update",
+		ResourceType: "Monitor",
+		ResourceID:   monitorID,
+	}
+	statusPageResource, err := bs.UpdateStatusPageResource(newStatusPageID, statusPageResourceID, reqBody)
+	assert.Nil(t, err)
+	assert.NotNil(t, statusPageResource)
+	assert.IsType(t, &uptime.StatusPageResource{}, statusPageResource)
+	assert.Equal(t, reqBody.PublicName, statusPageResource.Attributes.PublicName)
+}
+
+func TestDeleteStatusPageResource(t *testing.T) {
+	err := bs.DeleteStatusPageResource(newStatusPageID, statusPageResourceID)
+	assert.Nil(t, err)
+
+	err = bs.DeleteStatusPage(newStatusPageID)
+	assert.Nil(t, err)
+
+	err = bs.DeleteMonitor(monitorID)
+}
