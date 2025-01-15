b9fbe40423b2845e36f02f7fe55edf2204b81f58
Edoardo Rizzardi <e.rizzardi@truvity.com>
2025-01-15 17:22:23 +0100

    Add json tags to status updates response fields

--- models/status_updates_response.go
+++ models/status_updates_response.go
@@ -2,12 +2,12 @@ package models
 
 // StatusUpdates represents a collection of status updates with pagination information.
 type StatusUpdates struct {
-	Data       []StatusUpdate
-	Pagination Pagination
+	Data       []StatusUpdate `json:"data"`
+	Pagination Pagination     `json:"pagination"`
 }
 
 // StatusUpdate represents an individual status update with its attributes.
 type StatusUpdate struct {
 	Data
-	Attributes IncludedAttributes
+	Attributes IncludedAttributes `json:"attributes"`
 }
